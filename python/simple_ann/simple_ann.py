bb4e67c232726a8023f4c2986c180bb50e4994f2
Resha Dwika Hefni Al-Fahsi <resha.dwika.hefni.alfahsi@mail.ugm.ac.id>
2019-07-23 17:59:51 +0700

    update simple ann python

--- python/simple_ann/simple_ann.py
+++ python/simple_ann/simple_ann.py
@@ -1,6 +1,6 @@
 from numpy import exp, array, random, dot, genfromtxt
 training_set_inputs, training_set_outputs = genfromtxt('input.txt', delimiter=' '),  array([genfromtxt('output.txt', delimiter=' ')]).T
-synaptic_weights = 0.5 * random.random((len(training_set_inputs[0]),1)) - 1
+synaptic_weights = random.random((len(training_set_inputs[0]),1)) - 0.5
 for iteration in xrange(100000):
     output = 1 / (1 + exp(-(dot(training_set_inputs, synaptic_weights))))
     synaptic_weights += dot(training_set_inputs.T, (training_set_outputs - output) * output * (1 - output))
