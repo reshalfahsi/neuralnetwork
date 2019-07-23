aca6d891cf3c905f0e7129a63694e1eaf798cbbc
Resha Dwika Hefni Al-Fahsi <resha.dwika.hefni.alfahsi@mail.ugm.ac.id>
2019-07-23 14:51:53 +0700

    update python

--- python/simple_ann/simple_ann.py
+++ python/simple_ann/simple_ann.py
@@ -1,7 +1,7 @@
 from numpy import exp, array, random, dot, genfromtxt
 training_set_inputs, training_set_outputs = genfromtxt('input.txt', delimiter=' '),  array([genfromtxt('output.txt', delimiter=' ')]).T
-synaptic_weights = 2 * random.random((len(training_set_inputs[0]),1)) - 1
-for iteration in xrange(10000):
+synaptic_weights = 0.5 * random.random((len(training_set_inputs[0]),1)) - 1
+for iteration in xrange(100000):
     output = 1 / (1 + exp(-(dot(training_set_inputs, synaptic_weights))))
     synaptic_weights += dot(training_set_inputs.T, (training_set_outputs - output) * output * (1 - output))
 print ("True Output {}\nArtificial Neural Network Output {}".format(training_set_outputs.T,output.T))
