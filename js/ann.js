77f210c2b95523fe5ccabe5288ec9b05d2b34727
Resha Dwika Hefni Al-Fahsi <resha.dwika.hefni.alfahsi@mail.ugm.ac.id>
2019-07-23 14:40:43 +0700

    update js

--- js/ann.js
+++ js/ann.js
@@ -1,143 +1,255 @@
-// Source : https://franpapers.com/en/machine-learning-ai-en/2017-neural-network-implementation-in-javascript-by-an-example/
+// Source : https://github.com/morosanmihail/js-neural-network
 
-function Neuron(id, layer, biais) {
-    this.id = id;
-    this.layer = layer;
-    this.biais = biais || 0;
-    this.dropped = false;
-    this.output = undefined;
-    this.error = undefined;
-    this.activation = undefined;
-    this.derivative = undefined;
+//Neural Network
+
+function NeuralNetwork(inputcount, hiddenlayersize, outputcount) {
+	this.inputcount = inputcount;
+	this.hiddenlayersize = hiddenlayersize;
+	this.outputcount = outputcount;
+	
+	this.InputLayer = [];
+	this.HiddenLayer = [];
+	this.OutputLayer = [];
+	
+	this.Error = [];
+	
+	for(var i=0;i<=inputcount;i++)
+	{
+		this.InputLayer[i] = new Neuron();
+	}
+	this.InputLayer[0].SetOutput(1);
+	
+	for(var i=0;i<hiddenlayersize;i++)
+	{
+		this.HiddenLayer[i] = new Neuron();
+		this.HiddenLayer[i].SetRandomWeights(inputcount+1);
+	}
+	
+	for(var i=0;i<outputcount;i++)
+	{
+		this.OutputLayer[i] = new Neuron();
+		this.OutputLayer[i].SetRandomWeights(hiddenlayersize);
+	}
+}
+
+NeuralNetwork.prototype.RunDataSet = function(input) {
+	//Do checks here
+	for(var i=0;i<this.inputcount;i++)
+	{
+		if(isNaN(input[i]))
+		{
+			return [];
+		}
+	}
+	
+	//Run through network
+	for(var i=1;i<=this.inputcount;i++)
+	{
+		this.InputLayer[i].SetOutput(input[i-1]);
+	}
+	
+	for(var i=0;i<this.hiddenlayersize;i++)
+	{
+		this.HiddenLayer[i].CalculateOutput(this.InputLayer);
+	}
+	
+	var result = [];
+	
+	for(var i=0;i<this.outputcount;i++)
+	{
+		this.OutputLayer[i].CalculateOutput(this.HiddenLayer);
+		result[i] = this.OutputLayer[i].GetOutput();
+	}
+	
+	return result;
 };
-////////////////////////////////////////////
-function Network(params) {
-    // Required variables: lr, layers
-    this.lr = undefined; // Learning rate
-    this.layers = undefined;
-    this.hiddenLayerFunction = undefined; // activation function for hidden layer
-    this.neurons    = undefined;
-    this.weights    = undefined; 
-    
-    
-    // ... load params
-}
-////////////////////////////////////////////
-function randomBiais() {
-    return Math.random() * 2 - 1;
-}
-function randomWeight() {
-    return Math.random() * 2 - 1;
-}
-////////////////////////////////////////////
-// Input layer filling
-for (index = 0; index < this.layers[0]; index++)
-    this.neurons[index].output = inputs[index];
-// Fetching neurons from second layer (even if curr_layer equals 0, it'll be changed directly)
-for (index = this.layers[0]; index < this.nbNeurons; index++)
-{
-    neuron = this.neurons[index];
-    if (neuron.dropped)
-        continue;
-    // Update if necessary all previous layer neurons. It's a cache
-    if (prev_neurons === undefined || neuron.layer !== curr_layer)
-        prev_neurons = this.getNeuronsInLayer(curr_layer++);
-    // Computing w1*x1 + ... + wn*xn
-    for (sum = 0, n = 0, l = prev_neurons.length; n < l; n++) {
-        if (!prev_neurons[n].dropped)
-            sum += this.getWeight(prev_neurons[n], neuron) * prev_neurons[n].output;
-    }
-    // Updating output    
-    neuron.output = neuron.activation(sum + neuron.biais); 
-}
-// Output layer error computing: err = (expected-obtained)
-for (n = 0, l = outputs_neurons.length; n < l; n++)
-{
-    neuron = outputs_neurons[n];
-    grad = neuron.derivative(neuron.output);
-    err = targets[n] - neuron.output;
-    neuron.error = grad * err;
-    output_error += Math.abs(neuron.error);
-    // Update biais 
-    neuron.biais = neuron.biais + this.lr * neuron.error;        
-}
-this.outputError = output_error;
-// Fetching neurons from last layer
-for (index = this.layersSum[curr_layer-1] - 1; index >= 0; index--)
-{
-    neuron = this.neurons[index];
-    // Dropping neuron is a technique to add dynamic into training
-    if (neuron.dropped)
-        continue;
-    // Update if necessary all next layer neurons. It's a cache
-    if (next_neurons === undefined || neuron.layer !== curr_layer)
-        next_neurons = this.getNeuronsInLayer(curr_layer--);
-    // Computing w1*e1 + ... + wn*en
-    for (sum = 0, n = 0, l = next_neurons.length; n < l; n++) {
-        if (!next_neurons[n].dropped)
-            sum += this.getWeight(neuron, next_neurons[n]) * next_neurons[n].error;
-    }
-    // Updating error    
-    neuron.error = sum * neuron.derivative(neuron.output); 
-    this.globalError += Math.abs(neuron.error); 
-    // Update biais
-    neuron.biais = neuron.biais + this.lr * neuron.error;
-    // Updating weights w = w + lr * en * output
-    for (n = 0, l = next_neurons.length; n < l; n++)
-    {
-        if (next_neurons[n].dropped)
-            continue;
-        weight_index = this.getWeightIndex(neuron, next_neurons[n]); 
-        // Update current weight
-        weight = this.weightsTm1[weight_index] + this.lr * next_neurons[n].error * neuron.output;
-        // Update maxWeight (for visualisation)
-        max_weight = max_weight < Math.abs(weight) ? Math.abs(weight) : max_weight;
-        // Finally update weights
-        this.weights[weight_index] = weight;
-    }
+
+NeuralNetwork.prototype.TrainNetwork = function(input, desired) {
+	var res = this.RunDataSet(input);
+	
+	if(res.length == 0)
+	{
+		return [];
+	}
+	
+	for(var i=0;i<desired.length;i++)
+	{
+		this.Error[i] = desired[i] - res[i];
+	}
+	
+	for(var i=0;i<this.outputcount;i++)
+	{
+		this.OutputLayer[i].CalculateLocalGradientO(this.Error[i]);
+	}
+	
+	for(var i=0;i<this.hiddenlayersize;i++)
+	{
+		this.HiddenLayer[i].CalculateLocalGradientH(this.OutputLayer, i);
+	}
+	
+	for(var i=0;i<this.outputcount;i++)
+	{
+		this.OutputLayer[i].UpdateWeigthsFromGradient(this.HiddenLayer);
+	}
+	
+	for(var i=0;i<this.hiddenlayersize;i++)
+	{
+		this.HiddenLayer[i].UpdateWeigthsFromGradient(this.InputLayer);
+	}
+	
+	return res;
+};
+
+//Neuron
+
+function Neuron() {
+	this.Lambda = 0.9;
+	this.Eta = 0.9;
+	this.Alpha = 0.2;
+
+	this.Weights = [];
+	this.PrevWeightDelta = [];
+	
+	this.output = 0;
+	this.LocalGradient = 0;
 }
 
-////////////////////// Main thread:
-// Start web worker with training data through epochs
-worker.postMessage({
-    params: this.exportParams(),
-    weights: this.exportWeights(),
-    biais: this.exportBiais(),
-    training_data: training_data,
-    epochs: epochs
-});
-////////////////////// Worker:
-// Create copy of our current Network
-var brain = new Network(e.data.params);
-brain.weights = e.data.weights;
-// ...
-// Feedforward NN
-for (curr_epoch = 0; curr_epoch < epochs; curr_epoch++)
-{
-    for (sum = 0, i = 0; i < training_size; i++)
-    {
-        brain.feed(training_data[i].inputs);
-        brain.backpropagate(training_data[i].targets);
-        sum += brain.outputError;
-    }
-    
-    global_sum += sum;
-    mean = sum / training_size; 
-    global_mean = global_sum / ((curr_epoch+1) * training_size); 
-    // Send updates back to real thread
-    self.postMessage({
-        type: WORKER_TRAINING_PENDING,
-        curr_epoch: curr_epoch,
-        global_mean: global_mean,
-    });
+Neuron.prototype.SetWeights = function(weights) {
+	for(var i=0;i<weights.length;i++)
+	{
+		this.Weights[i] = weights[i];
+		this.PrevWeightDelta[i] = 0;
+	}
 }
-/////////////////////// Main thread:
-// Training is over: we update our weights and biais
-if (e.data.type === WORKER_TRAINING_OVER)
-{
-    that.importWeights( e.data.weights );
-    that.importBiais( e.data.biais );
-    // Feeding and bping in order to have updated values (as error) into neurons or others
-    that.feed( training_data[0].inputs );
-    that.backpropagate( training_data[0].targets );
+
+Neuron.prototype.SetRandomWeights = function(count) {
+	for(var i=0;i<count;i++)
+	{
+		var w = Math.random();
+		this.Weights[i] = w;
+		this.PrevWeightDelta[i] = 0;
+	}
+};
+
+Neuron.prototype.SetOutput = function(output) {
+	this.output = output;
+};
+
+Neuron.prototype.GetOutput = function() {
+	return this.output;
+};
+
+Neuron.prototype.CalculateOutput = function(previousLayer) {
+	var result = 0.0;
+	
+	for(var i=0;i<previousLayer.length;i++)
+	{
+		result += previousLayer[i].GetOutput() * this.Weights[i];
+	}
+	
+	if(result < -45.0)
+		result = 0;
+	else if(result > 45.0)
+		result = 1;
+	else
+		result = 1.0 / ( 1.0 + Math.exp(-result * this.Lambda) );
+		
+	this.output = result;
+};
+
+Neuron.prototype.GetLocalGradient = function() {
+	return this.LocalGradient;
+};
+
+Neuron.prototype.CalculateLocalGradientO = function(error) {
+	this.LocalGradient = this.Lambda * this.output * (1-this.output) * error;
+};
+
+Neuron.prototype.CalculateLocalGradientH = function(nextLayer, index) {
+	this.LocalGradient = this.Lambda * this.output * (1-this.output);
+	
+	var r = 0.0;
+	for(var i=0;i<nextLayer.length;i++)
+	{
+		r += nextLayer[i].GetLocalGradient() * nextLayer[i].Weights[index];
+	}
+	
+	this.LocalGradient *= r;
+};
+
+Neuron.prototype.UpdateWeigthsFromGradient = function(previousLayer) {
+	for(var i=0;i<this.Weights.length;i++)
+	{
+		var delta = this.Eta * this.LocalGradient * previousLayer[i].GetOutput();
+		
+		this.Weights[i] = this.Weights[i] + delta + this.Alpha * this.PrevWeightDelta[i];
+		
+		this.PrevWeightDelta[i] = delta;
+	}
+};
+
+//Random Utilities
+
+NeuralNetwork.prototype.SaveToLocalStorage = function() {
+	if(supports_html5_storage)
+	{
+		localStorage["OutputLayer"] = JSON.stringify(this.OutputLayer);
+		localStorage["HiddenLayer"] = JSON.stringify(this.HiddenLayer);
+	}
+}
+
+NeuralNetwork.prototype.LoadFromLocalStorage = function() {
+	if(supports_html5_storage)
+	{
+		var OL = JSON.parse(localStorage["OutputLayer"]);
+		var HL = JSON.parse(localStorage["HiddenLayer"]);
+		
+		for(var i=0;i<this.outputcount;i++)
+		{
+			this.OutputLayer[i] = new Neuron();
+			this.OutputLayer[i].SetWeights(OL[i].Weights);
+		}
+		
+		for(var i=0;i<this.hiddenlayersize;i++)
+		{
+			this.HiddenLayer[i] = new Neuron();
+			this.HiddenLayer[i].SetWeights(HL[i].Weights);
+		}
+	}
+}
+
+function supports_html5_storage() {
+  try {
+    return 'localStorage' in window && window['localStorage'] !== null;
+  } catch (e) {
+    return false;
+  }
+}
+
+
+//Web Worker
+
+function runRandomData(i,TCount,visual,targetFunction) {	
+	//for(var i=0;i<TCount;i++)
+	{
+		var input1 = Math.random();
+		var input2 = Math.random();
+						
+		var input = [input1, input2];
+						
+		var desired = [targetFunction(input1, input2)];
+						
+		var res = NN.TrainNetwork(input, desired);
+		
+		$("#input1").val(input1);
+		$("#input2").val(input2);
+							
+		$("#output").html("Training " + i + ": " + desired + " - " + res);
+		
+		
+			if(i<TCount && visual)
+			{
+				setTimeout(function() { runRandomData(i+1,TCount,visual, targetFunction)},0);
+			}
+	}
 }
