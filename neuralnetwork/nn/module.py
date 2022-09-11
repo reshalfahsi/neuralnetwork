5a8b747fefc518f80eb0c8c2840892a93822bdb8
reshalfahsi <resha.alfahsi@gmail.com>
2022-09-11 12:32:23 +0700

    fix dimension issue in optimization

--- neuralnetwork/nn/module.py
+++ neuralnetwork/nn/module.py
@@ -105,12 +105,12 @@ class Linear(Module):
             assert (
                 "grad2_A" in self.__dict__
             ), "Second order derivative has not been calculated yet."
-            denum_A = self.grad2_A
+            denum_A = self.grad2_A.mean(axis=0, keepdims=True)
             if self.bias:
-                denum_b = self.grad2_b
+                denum_b = self.grad2_b.mean(axis=0, keepdims=True)
             lr = 1.0
             epsilon = 1e-8
 
-        self.A -= lr * self.grad_A / (denum_A + epsilon)
+        self.A -= lr * self.grad_A.mean(axis=0, keepdims=True) / (denum_A + epsilon)
         if self.bias:
-            self.b -= lr * self.grad_b / (denum_b + epsilon)
+            self.b -= lr * self.grad_b.mean(axis=0, keepdims=True) / (denum_b + epsilon)
