faf89b643e17ece36d557f70c53f51fe8c9a8d04
reshalfahsi <resha.alfahsi@gmail.com>
2023-04-03 08:55:36 +0700

    update notebook and examples

--- examples/classification.py
+++ examples/classification.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 """Classification.ipynb
+"""
 
-"""  ## **Hyperparameters**"""
+"""## **Hyperparameters**"""
 
 NUM_EPOCHS = 32
 BATCH_SIZE = 16
@@ -17,8 +18,8 @@ from neuralnetwork import ds
 import numpy as np
 
 
-train_dataset = PneumoniaMNIST(split="train", download=True)
-test_dataset = PneumoniaMNIST(split="test", download=True)
+train_dataset = PneumoniaMNIST(split='train', download=True)
+test_dataset = PneumoniaMNIST(split='test', download=True)
 
 print("Train Dataset:", len(train_dataset))
 print("Test Dataset", len(test_dataset))
@@ -52,67 +53,57 @@ class NeuralNetwork(nn.Module):
         self.out5 = self.activation(self.out4)
 
         return self.out5
-
+    
     def backward(self, lr, criterion, method=None):
-        # Computational Graph
-        #
-        self.dx0 = criterion.grad()  # loss_grad(pred, y)
-        #        |
-        self.dx1 = self.sigmoid.grad(self.out4)  # sigmoid_grad(pred)
-        #        |
-        #        +
-        #       / \
-        #      |   |
-        #  b_grad  *
-        #         / \
-        #        |   |
-        self.dx2 = self.linear2.grad(self.dx1 * self.dx0)  #   A_grad   x_grad
-        #          .
-        self.dx3 = self.sigmoid.grad(self.out2)  #          .
-        self.dx4 = self.linear1.grad(self.dx3 * self.dx2)  #          .
+                                                               
+        self.dx0 = criterion.grad()                            
+        self.dx1 = self.activation.grad(self.out4)             
+        self.dx2 = self.linear2.grad(self.dx1 * self.dx0)      
 
-        self.dx5 = self.sigmoid.grad(self.out0)
+        self.dx3 = self.activation.grad(self.out2)            
+        self.dx4 = self.linear1.grad(self.dx3 * self.dx2) 
+
+        self.dx5 = self.activation.grad(self.out0)
         self.dx6 = self.linear0.grad(self.dx5 * self.dx4)
 
-        if method == "newton":
-            self.d2x0 = criterion.grad("hessian")
-            self.d2x1 = self.sigmoid.grad(self.out4, "hessian")
+        if method == 'newton':
+            self.d2x0 = criterion.grad('hessian')                                                        
+            self.d2x1 = self.activation.grad(self.out4, 'hessian')
 
             gradient = {
-                "error_first": self.dx0,
-                "error_second": self.d2x0,
-                "nonlinearity_first": self.dx1,
-                "nonlinearity_second": self.d2x1,
-            }
+                'error_first': self.dx0,
+                'error_second': self.d2x0,
+                'nonlinearity_first': self.dx1,
+                'nonlinearity_second': self.d2x1,
+            }               
 
-            self.d2x2 = self.linear2.grad(gradient, "hessian")
-            self.d2x3 = self.sigmoid.grad(self.out2, "hessian")
-
-            gradient = {
-                "error_first": self.dx2,
-                "error_second": self.d2x2,
-                "nonlinearity_first": self.dx3,
-                "nonlinearity_second": self.d2x3,
-            }
-
-            self.d2x4 = self.linear1.grad(gradient, "hessian")
-            self.d2x5 = self.sigmoid.grad(self.out0, "hessian")
+            self.d2x2 = self.linear2.grad(gradient, 'hessian')
+            self.d2x3 = self.activation.grad(self.out2, 'hessian') 
 
             gradient = {
-                "error_first": self.dx4,
-                "error_second": self.d2x4,
-                "nonlinearity_first": self.dx5,
-                "nonlinearity_second": self.d2x5,
-            }
+                'error_first': self.dx2,
+                'error_second': self.d2x2,
+                'nonlinearity_first': self.dx3,
+                'nonlinearity_second': self.d2x3,
+            }         
+                                                                            
+            self.d2x4 = self.linear1.grad(gradient, 'hessian')
+            self.d2x5 = self.activation.grad(self.out0, 'hessian')
 
-            self.d2x6 = self.linear0.grad(gradient, "hessian")
+            gradient = {
+                'error_first': self.dx4,
+                'error_second': self.d2x4,
+                'nonlinearity_first': self.dx5,
+                'nonlinearity_second': self.d2x5,
+            } 
+
+            self.d2x6 = self.linear0.grad(gradient, 'hessian')
 
         self.linear0.update(lr, method)
         self.linear1.update(lr, method)
         self.linear2.update(lr, method)
 
-
-"""## **Training**"""
+"""## **Utilities**"""
 
 import time
 import matplotlib.pyplot as plt
@@ -125,11 +116,13 @@ def accuracy(model, X, Y):
     acc = acc / Y.shape[0]
     return acc
 
-
 seed = np.random.randint(2147483647)
 print(seed)
 
-"""### **Gradient Descent**"""
+"""## **Gradient Descent**
+
+### **Training**
+"""
 
 train_loader = ds.get_loader(dataset=train_dataset, batch_size=BATCH_SIZE)
 test_loader = ds.get_loader(dataset=test_dataset, batch_size=1)
@@ -170,7 +163,7 @@ for epoch in range(NUM_EPOCHS):
 
     loss_train.append(np.array(loss).mean())
 
-    if np.array(acc).mean() > 0.9:
+    if np.array(acc).mean() > 0.95:
         break
     print("-----------------------------------------------------------")
 end = time.perf_counter()
@@ -178,14 +171,61 @@ end = time.perf_counter()
 print(f"Training finished in {epoch + 1} epochs and {end - start:0.4f} seconds")
 
 plt.title("Gradient Descent")
-plt.plot(loss_train, color="r")
+plt.plot(loss_train, color = 'r')
 plt.xlabel("epoch")
 plt.ylabel("loss")
 plt.grid()
 plt.show()
 plt.clf()
 
-"""### **Newton Method**"""
+"""### **Testing**"""
+
+import random
+from IPython.display import display
+
+
+acc = list()
+fps = list()
+
+# Evaluation
+for idx, pack in enumerate(test_loader):
+     start = time.perf_counter()
+     x, y = pack
+     bs = x.shape[0]
+     L = x.shape[1] * x.shape[2]
+     x = x.reshape(bs, 1, L) / 255.0
+     y = y.reshape(bs, 1, 1)
+     acc.append(accuracy(model, x, y))
+     end = time.perf_counter()
+     fps.append(1./(end - start))
+
+     if idx >= int(len(test_dataset) / BATCH_SIZE) - 1:
+         break
+
+
+print(f"Accuracy on testing: {round(np.array(acc).mean() * 100., 2)}%")
+print(f"FPS: {round(np.array(fps).mean(), 2)}")
+
+# Sample
+index = random.randint(0, len(test_dataset))
+
+x, y = test_dataset[index]
+display(x.resize((140, 140)))
+x = np.array(x)
+L = x.shape[0] * x.shape[1]
+x = x.reshape(1, 1, L)/255.
+pred = model(x)
+
+pred = pred.squeeze(0).squeeze(0)
+pred[pred>=0.5] = 1
+pred[pred<0.5] = 0
+print("Prediction: Pneumonia" if pred[0] else "Prediction: Healthy")
+print("Ground Truth: Pneumonia" if y[0] else "Ground Truth: Healthy")
+
+"""## **Newton Method**
+
+### **Training**
+"""
 
 train_loader = ds.get_loader(dataset=train_dataset, batch_size=BATCH_SIZE)
 test_loader = ds.get_loader(dataset=test_dataset, batch_size=1)
@@ -209,7 +249,7 @@ for epoch in range(NUM_EPOCHS):
         y = y.reshape(bs, 1, 1)
         pred = model(x)
         loss.append(criterion(pred, y))
-        model.backward(lr, criterion, "newton")
+        model.backward(lr, criterion, 'newton')
         acc.append(accuracy(model, x, y))
         if idx % 20 == 0 or idx == len(train_dataset) - 1:
             print(
@@ -226,7 +266,7 @@ for epoch in range(NUM_EPOCHS):
 
     loss_train.append(np.array(loss).mean())
 
-    if np.array(acc).mean() > 0.9:
+    if np.array(acc).mean() > 0.95:
         break
     print("-----------------------------------------------------------")
 end = time.perf_counter()
@@ -234,29 +274,53 @@ end = time.perf_counter()
 print(f"Training finished in {epoch + 1} epochs and {end - start:0.4f} seconds")
 
 plt.title("Newton Method")
-plt.plot(loss_train, color="b")
+plt.plot(loss_train, color = 'b')
 plt.xlabel("epoch")
 plt.ylabel("loss")
 plt.grid()
 plt.show()
 plt.clf()
 
-"""## **Testing**"""
+"""### **Testing**"""
 
 import random
 from IPython.display import display
 
+
+acc = list()
+fps = list()
+
+# Evaluation
+for idx, pack in enumerate(test_loader):
+     start = time.perf_counter()
+     x, y = pack
+     bs = x.shape[0]
+     L = x.shape[1] * x.shape[2]
+     x = x.reshape(bs, 1, L) / 255.0
+     y = y.reshape(bs, 1, 1)
+     acc.append(accuracy(model, x, y))
+     end = time.perf_counter()
+     fps.append(1./(end - start))
+
+     if idx >= int(len(test_dataset) / BATCH_SIZE) - 1:
+         break
+
+
+print(f"Accuracy on testing: {round(np.array(acc).mean() * 100., 2)}%")
+print(f"FPS: {round(np.array(fps).mean(), 2)}")
+
+# Sample
 index = random.randint(0, len(test_dataset))
 
 x, y = test_dataset[index]
 display(x.resize((140, 140)))
 x = np.array(x)
 L = x.shape[0] * x.shape[1]
-x = x.reshape(1, 1, L) / 255.0
+x = x.reshape(1, 1, L)/255.
 pred = model(x)
 
 pred = pred.squeeze(0).squeeze(0)
-pred[pred >= 0.5] = 1
-pred[pred < 0.5] = 0
+pred[pred>=0.5] = 1
+pred[pred<0.5] = 0
 print("Prediction: Pneumonia" if pred[0] else "Prediction: Healthy")
 print("Ground Truth: Pneumonia" if y[0] else "Ground Truth: Healthy")
